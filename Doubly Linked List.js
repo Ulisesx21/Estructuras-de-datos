db2cb2c01fb0cdbe643e95c323adcb12fdd00443
Ulisesx21 <75042766+Ulisesx21@users.noreply.github.com>
2022-07-29 23:45:03 -0300

    -

--- Doubly Linked List.js
+++ Doubly Linked List.js
@@ -1,50 +1,50 @@
-class Node{
-    constructor(value){
-      this.value = value
-      this.prev = null
-      this.next = null
+class Node {
+  constructor(value) {
+    this.value = value
+    this.prev = null
+    this.next = null
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+
+  append(value) {
+    let newNode = new Node(value)
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode
+      newNode.prev = this.tail
+      this.tail = newNode
+    }
+    this.length++
+  }
+
+  prepend(value) {
+    let newNode = new Node(value)
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head
+      this.head.prev = newNode
+      this.head = newNode
+    }
+    this.length++
+  }
+
+  printList() {
+    let aux = this.head
+    while (aux) {
+      console.log(aux.value)
+      aux = aux.next
     }
   }
-  
-  class DoublyLinkedList{
-    constructor(){
-      this.head = null
-      this.tail = null
-      this.length = 0
-    }
-    
-    append(value){
-      let newNode = new Node(value)
-      if(this.head === null){
-        this.head = newNode;
-        this.tail = newNode;
-      }else{
-        this.tail.next = newNode
-        newNode.prev = this.tail
-        this.tail = newNode
-      }
-      this.length++
-    }
-    
-    prepend(value){
-      let newNode = new Node(value)
-      if(this.head === null){
-        this.head = newNode;
-        this.tail = newNode;
-      }else{
-        newNode.next = this.head
-        this.head.prev = newNode
-        this.head = newNode
-      }
-      this.length++
-    }
-    
-    printList(){
-      let aux = this.head
-      while(aux){
-        console.log(aux.value)
-        aux = aux.next
-      }
-    }
-    
-  }
+
+}
--- Linked List.js
+++ Linked List.js
@@ -1,69 +1,85 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
+class Node{
+    constructor(value){
+      this.value = value;
+      this.next = null;
     }
-}
-
-class LinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
+  }
+  
+  class LinkedList{
+    constructor(){
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
     }
-
-    append(value) {
-        let newNode = new Node(value)
-        if (this.head) {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        } else {
-            this.head = newNode;
-            this.tail = newNode;
-        }
-        this.length++
-    }
-
-    prepend(value) {
-        let newNode = new Node(value)
-        newNode.next = this.head;
+    
+    append(value){
+      let newNode = new Node(value)
+      if(this.head){
+        this.tail.next = newNode;
+        this.tail = newNode;
+      }else{
         this.head = newNode;
-        this.length++
+        this.tail = newNode;
+      }
+      this.length++
     }
-
-    printList() {
-        let aux = this.head;
-        if (this.head === null) return "Empty"
-        while (aux) {
-            console.log(aux.value)
-            aux = aux.next
+    
+    prepend(value){
+      let newNode = new Node(value)
+      newNode.next = this.head;
+      this.head = newNode;
+      this.length++
+    }
+    
+    printList(){
+      let aux = this.head;
+      if(this.head === null) return "Empty"
+      while(aux){
+        console.log(aux.value)
+        aux = aux.next
+      }
+    }
+    
+    reverse(){
+      let prev = null
+      while(this.head){
+        let nextNode = this.head.next
+        this.head.next = prev
+        prev = this.head
+        this.head = nextNode
+      }
+          return prev
+    }
+    
+    arrToList(arr){
+      for(let i = 0; i < arr.length; i++){
+        let newNode = new Node(arr[i])
+        if(this.head === null){
+          this.head = newNode;
+          this.tail = newNode;
+          this.length++
+        }else{
+          this.tail.next = newNode;
+          this.tail = newNode;
+          this.length++
         }
+      }
     }
-
-    reverse() {
-        let prev = null
-        while (this.head) {
-            let nextNode = this.head.next
-            this.head.next = prev
-            prev = this.head
-            this.head = nextNode
+    
+    removeK(value){
+      let aux = this.head
+      while(aux.value === value){
+        aux = aux.next
+      }
+      let current = aux
+      while(current.next){
+        if(current.next.value === value){
+          current.next = current.next.next
+        }else{
+          current = current.next
         }
-        return prev
+      }
+      this.head = aux
     }
-
-    arrToList(arr) {
-        for (let i = 0; i < arr.length; i++) {
-            let newNode = new Node(arr[i])
-            if (this.head === null) {
-                this.head = newNode;
-                this.tail = newNode;
-                this.length++
-            } else {
-                this.tail.next = newNode;
-                this.tail = newNode;
-                this.length++
-            }
-        }
-    }
-
-}
+    
+  }
